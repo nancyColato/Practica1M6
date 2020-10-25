664d3f9f2b65aedb022f8ee4619f3851d10bf4d4
nancycmartinez <a_dramartinez@hotmail.com>
2020-10-25 10:35:04 -0600

    segundo archivo

---
+++ integrantes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integrantes</title>
+</head>
+<body>
+    <?php 
+     echo "
+    <div style='margin:5%'>
+    <table >
+        <thead>
+            <tr>
+                <th>N°</th>
+                <th>Nombre Completo</th>
+                <th>Codigo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Nancy Alondra Colato Martínez</td>
+                <td>U20150457</td>
+            </tr>
+        </tbody>
+    </table> <div>";
+    ?>
+</body>
+</html>
