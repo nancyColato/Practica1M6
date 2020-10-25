2ffc10e054818ccd589f98c6ff369e5006328be3
nancycmartinez <a_dramartinez@hotmail.com>
2020-10-25 10:25:05 -0600

    estructura

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "<h1 style='text-align:center'>BIENVENIDOS</h1>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 1 M6</title>
+</head>
+<body>
+    <?php
+        echo "<h1 style='text-align:center'>BIENVENIDOS</h1>";
+    ?>
+    
+</body>
+</html>
