4137e233579dd48eb14ecb7f391893430ca8f6d6
Fidel García <f.garcia@abposus.com>
2020-10-25 11:01:56 -0600

    Change by Fidel U20151154
    Realice cambios Agregando mi Carnet y Nombre ademas  estilo a los enlaces

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practica 1 M6</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php
         echo "<h1 style='text-align:center'>BIENVENIDOS</h1>";
     ?>
-   <a href="integrantes.php" type="button" style="text-align:center">Integrantes</a>
+   <a href="integrantes.php" type="button" class="btnM6">Integrantes</a>
 </body>
 </html>
