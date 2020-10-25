d7ceb619016835215196b8ffb8e166f78ccf319c
nancycmartinez <a_dramartinez@hotmail.com>
2020-10-25 15:31:40 -0600

    original

--- integrantes.php
+++ integrantes.php
@@ -4,25 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Integrantes</title>
-
-    <style>
-        table {
-        width: 100%;
-        }
-
-        th {
-        height: 50px;
-        background-color: #000;
-        color: white;
-        }
-        tr{
-            text-align: center;  
-        }
-        td{ height: 40px;}
-        tr:nth-child(even) {background-color: #f2f2f2;}
-        tr:hover {background-color: #f5f5f5;}
-    </style>
-
 </head>
 <body>
     <?php 
